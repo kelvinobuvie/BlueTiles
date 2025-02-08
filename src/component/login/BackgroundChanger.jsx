5b380dbc222bd5e5e3b9479904f8525c48d7c700
kelvinobuvie <kelvinobuvie@gmail.com>
2025-02-09 00:54:08 +0100

    changed the name on the login page

--- src/component/login/BackgroundChanger.jsx
+++ src/component/login/BackgroundChanger.jsx
@@ -28,7 +28,7 @@ class BackgroundChanger extends Component {
     return (
       <div className="background-container flex flex-col gap-3 justify-center items-center  bg-gray-300 p-12">
           <h1 className='text-white font-light text-5xl z-1000 border-b-8 border-orange-600 px-2 py-2'>
-            Blue<span className='font-medium text-blue-950'>Tiles</span>
+            <span className='font-medium text-blue-950'>quantra</span>
           </h1>
           <p className='text-lg flex text-center text-white'>Empower your finances effortlessly: set goals, track spending, and lock in savings with a smart dashboard that keeps you on target and helps build better financial habits</p>
       </div>
